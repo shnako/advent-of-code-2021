df18f5e94a585858758bd1c5ff5b3fc7c7c1d95d
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-01 08:05:56 +0000

    Added template

---
+++ src/template/solution.py
@@ -0,0 +1,14 @@
+from src.util.file_input_processor import *
+
+
+def part_1():
+    inp = read_integers()
+
+
+def part_2():
+    inp = read_integers()
+
+
+if __name__ == "__main__":
+    print(f'Part 1 solution: {part_1()}')
+    print(f'Part 2 solution: {part_2()}')
