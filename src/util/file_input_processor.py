eb7abee9d9da10e90dc02cb61fbfc31baa581611
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-01 08:00:18 +0000

    Added file_input_processor.py

---
+++ src/util/file_input_processor.py
@@ -0,0 +1,16 @@
+INPUT_FILE_NAME = "input.txt"
+
+
+def read_text():
+    with open(INPUT_FILE_NAME) as f:
+        return f.read()
+
+
+def read_lines():
+    with open(INPUT_FILE_NAME) as f:
+        return f.readlines()
+
+
+def read_integers():
+    input_lines = read_lines()
+    return [int(i) for i in input_lines]
