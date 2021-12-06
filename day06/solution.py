c9d3e37b1a1b2966396e5c79bc1e8dc8c8decdfc
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-06 10:04:08 +0000

    Added day 6 solution

---
+++ day06/solution.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+from util.file_input_processor import *
+
+"""
+This solution is based on the observation that the number of fish is going to be extremely large after not many days,
+but the number of days we care about is limited to 8.
+Therefore, instead of having a list of all the fish, we use a deque of the number of fish that will spawn by day.
+The number at the front of the deque is the number of fish that will spawn in 0 days and in 8 days at the back.
+Every day we pop the number of fish at the left (day 0 - the fish ready to reproduce), moving all the spawn days down.
+We add them to the number of fish ready to reproduce after 6 days and set their offspring (same number) as
+the fish that will reproduce after 8 days.
+This way we can efficiently simulate any number of days so the solutions to part 1 and part 2 become the same.
+"""
+
+
+def read_spawn_queue():
+    fish_spawn_days = read_integer_line()
+    spawn_days = [0] * 9
+    for fish in fish_spawn_days:
+        spawn_days[fish] += 1
+    return deque(spawn_days)
+
+
+def simulate_spawn_days(spawn_q, days):
+    for day in range(days):
+        spawned_fish = spawn_q.popleft()
+        spawn_q[6] += spawned_fish
+        spawn_q.append(spawned_fish)
+
+
+def part_1():
+    spawn_q = read_spawn_queue()
+    simulate_spawn_days(spawn_q, 80)
+    return sum(spawn_q)
+
+
+def part_2():
+    spawn_q = read_spawn_queue()
+    simulate_spawn_days(spawn_q, 256)
+    return sum(spawn_q)
+
+
+if __name__ == "__main__":
+    print(f'Part 1 solution: {part_1()}')
+    print(f'Part 2 solution: {part_2()}')
