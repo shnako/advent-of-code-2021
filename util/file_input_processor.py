261b8cc646f5f9422c47c271763717e6d0d31c80
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-06 08:28:47 +0000

    Added read_integer_line to be used when all numbers are on 1 line

--- util/file_input_processor.py
+++ util/file_input_processor.py
@@ -14,3 +14,8 @@ def read_lines():
 def read_integer_lines():
     input_lines = read_lines()
     return [int(i) for i in input_lines]
+
+
+def read_integer_line():
+    input_lines = read_lines()
+    return [int(i) for i in input_lines[0].split(",")]
