24bf7f6c172a1f0f705ef3d5ac61904de8f3cee2
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-13 15:56:00 +0000

    Added day 12 solution

---
+++ day12/solution.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+
+from util.file_input_processor import *
+
+"""
+This solution keeps track of each cave's adjacent caves in a dict
+and uses a Depth-First Search algorithm to navigate all the valid paths and recursively sum them up.
+The solution to the second part keeps track of the revisited cave
+whereas the solution to the first part overwrites it from the beginning to prevent revisiting.
+"""
+
+START = 'start'
+END = 'end'
+
+global adjacent_caves
+
+
+def read_input():
+    lines = read_lines()
+
+    global adjacent_caves
+    adjacent_caves = defaultdict(list)
+    for line in lines:
+        cave1, cave2 = line.split('-', 1)
+        adjacent_caves[cave1].append(cave2)
+        adjacent_caves[cave2].append(cave1)
+
+
+def find_valid_paths(path, current_cave, revisited_cave):
+    if current_cave == END:
+        return 1
+
+    if current_cave.islower() and current_cave in path:
+        if current_cave == START or revisited_cave:
+            return 0
+        else:
+            revisited_cave = current_cave
+
+    return sum(map(lambda adjacent_cave: find_valid_paths(path + [current_cave], adjacent_cave, revisited_cave),
+                   adjacent_caves[current_cave]))
+
+
+def part_1():
+    read_input()
+    return find_valid_paths([], START, "revisiting not allowed")
+
+
+def part_2():
+    read_input()
+    return find_valid_paths([], START, None)
+
+
+if __name__ == "__main__":
+    print(f'Part 1 solution: {part_1()}')
+    print(f'Part 2 solution: {part_2()}')
