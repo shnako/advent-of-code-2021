2fe602b636066b27d895b5b5ff0957256913a845
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2021-12-14 14:56:28 +0000

    Added day 14 solution

---
+++ day14/solution.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+
+from util.file_input_processor import *
+
+"""
+This solution relies on the observation that on each step,
+each polymer pair splits into 2 polymer pairs and adds 1 new element to the polymer.
+Therefore, we only need to keep track of how many of each pair we have and how many elements have been added.
+On each step we go through all the pairs that have already occurred and split them,
+resulting in the same number of the 2 resulting pairs being added, along with the same number of the new element.
+"""
+
+
+def read_input():
+    lines = read_lines()
+
+    template = lines[0]
+    rules = dict(line.split(' -> ') for line in lines[2:])
+
+    return template, rules
+
+
+def create_element_occurrence_dict(polymer_template):
+    occurrences = defaultdict(lambda: 0)
+    for element in polymer_template:
+        occurrences[element] += 1
+    return occurrences
+
+
+def create_pair_occurrence_dict(polymer_template):
+    occurrences = defaultdict(lambda: 0)
+    for i in range(1, len(polymer_template)):
+        occurrences[polymer_template[i - 1] + polymer_template[i]] += 1
+    return occurrences
+
+
+def execute_step(element_occurrences, pair_occurrences, rules):
+    new_pair_occurrences = defaultdict(lambda: 0)
+    for polymer in pair_occurrences.keys():
+        element_occurrences[rules[polymer]] += pair_occurrences[polymer]
+        new_pair_occurrences[polymer[0] + rules[polymer]] += pair_occurrences[polymer]
+        new_pair_occurrences[rules[polymer] + polymer[1]] += pair_occurrences[polymer]
+    return element_occurrences, new_pair_occurrences
+
+
+def get_result_after_steps(steps):
+    polymer_template, rules = read_input()
+    element_occurrences = create_element_occurrence_dict(polymer_template)
+    pair_occurrences = create_pair_occurrence_dict(polymer_template)
+
+    for _ in range(steps):
+        element_occurrences, pair_occurrences = execute_step(element_occurrences, pair_occurrences, rules)
+
+    least_common_element = min(element_occurrences, key=element_occurrences.get)
+    most_common_element = max(element_occurrences, key=element_occurrences.get)
+
+    return element_occurrences[most_common_element] - element_occurrences[least_common_element]
+
+
+def part_1():
+    return get_result_after_steps(10)
+
+
+def part_2():
+    return get_result_after_steps(40)
+
+
+if __name__ == "__main__":
+    print(f'Part 1 solution: {part_1()}')
+    print(f'Part 2 solution: {part_2()}')
---
+++ day14/test_solution.py
@@ -0,0 +1,11 @@
+import unittest
+
+from solution import part_1, part_2
+
+
+class TestSolution(unittest.TestCase):
+    def test_part_1(self):
+        self.assertEqual(part_1(), 2010)
+
+    def test_part_2(self):
+        self.assertEqual(part_2(), 2437698971143)
