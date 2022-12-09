1f8295422eb25841322452727e5c31dc838cb428
Vlad Schnakovszki <shnako@yahoo.com>
2022-12-09 10:34:21 +0000

    Added day 25 solution.

--- day25/solution.py
+++ day25/solution.py
@@ -3,16 +3,70 @@ from time import time
 from util.file_input_processor import *
 
 
-# I coded the solution to part 1 but didn't push it and it was lost on my old work laptop.
-# Maybe one day I will have the time to rewrite it.
-# Part 2 simply requires all the other problems to be solved.
+# I coded the solution to part 1 in 2021 on my work laptop but didn't push it as I wanted to push both parts together.
+# Since then, I switched jobs and lost the solution when my laptop was wiped.
+# During AoC 2022 I finished all the remaining problems and coded this one again for completeness.
+
+# For part 1, we simulate the movements until no more movements are possible.
+# We look at all eastbound urchins, determine if they can move and create a matrix with the same indices,
+# containing True if they can move and False if not.
+# We then use this map to move all the eastbound urchins, after which we do the same thing with the southbound ones.
+# Once we determine that no urchins can move (no True value in either matrix), we return the result (number of moves).
+
+# For part 2, we simply need to have solved all the previous problems to get the star so nothing to code.
 
 def part_1():
-    inp = read_integer_lines()
+    cucumbers = [list(line) for line in read_lines()]
+
+    movements = 0
+    while True:
+        east_movement_matrix = get_movement_matrix(cucumbers, ">")
+        move(cucumbers, east_movement_matrix)
+        south_movement_matrix = get_movement_matrix(cucumbers, "v")
+        move(cucumbers, south_movement_matrix)
+
+        movements += 1
+        if not any(True in sublist for sublist in east_movement_matrix + south_movement_matrix):
+            break
+
+    return movements
 
 
 def part_2():
-    inp = read_integer_lines()
+    return "Merry Christmas!"
+
+
+def get_movement_matrix(cucumbers, direction):
+    movement_array = list()
+
+    for i in range(len(cucumbers)):
+        movement_line = list()
+
+        for j in range(len(cucumbers[i])):
+            if direction == cucumbers[i][j] == ">" and cucumbers[i][nexti(j, cucumbers[i])] == ".":
+                movement_line.append(True)
+            elif direction == cucumbers[i][j] == "v" and cucumbers[nexti(i, cucumbers)][j] == ".":
+                movement_line.append(True)
+            else:
+                movement_line.append(False)
+
+        movement_array.append(movement_line)
+
+    return movement_array
+
+
+def move(cucumbers, movement_array):
+    for i in range(len(cucumbers)):
+        for j in range(len(cucumbers[i])):
+            if movement_array[i][j]:
+                if cucumbers[i][j] == ">":
+                    cucumbers[i][j], cucumbers[i][nexti(j, cucumbers[i])] = cucumbers[i][nexti(j, cucumbers[i])], cucumbers[i][j]
+                elif cucumbers[i][j] == "v":
+                    cucumbers[i][j], cucumbers[nexti(i, cucumbers)][j] = cucumbers[nexti(i, cucumbers)][j], cucumbers[i][j]
+
+
+def nexti(index, lst):
+    return index + 1 if index + 1 < len(lst) else 0
 
 
 if __name__ == "__main__":
--- day25/test_solution.py
+++ day25/test_solution.py
@@ -5,7 +5,7 @@ from solution import part_1, part_2
 
 class TestSolution(TestCase):
     def test_part_1(self):
-        self.assertEqual(part_1(), None)
+        self.assertEqual(part_1(), 489)
 
     def test_part_2(self):
-        self.assertEqual(part_2(), None)
+        self.assertEqual(part_2(), "Merry Christmas!")
